55061f9e5e1b8fbf9c6de5fda1609bf8ddbf4c02
SAROME <csarome@TMOVILES.COM.AR>
2020-05-31 20:49:30 -0300

    primer commit

---
+++ obtener_cotizacion.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+#from webdriver_manager.firefox import GeckoDriverManager
+from pathlib import Path
+import json
+import re
+import datetime
+import requests
+
+
+def cotizacion_cedears(driver, base_url):
+
+
+    driver.get(base_url + 'Mercado/Cotizaciones')
+
+    elem = Select(driver.find_element_by_id('paneles'))
+    elem.select_by_visible_text('CEDEARs')
+
+    wait = WebDriverWait(driver, 10)
+    expected_text = 'Acciones Argentina - Panel CEDEARs'
+    selector = (By.ID, 'header-cotizaciones')
+    condition = EC.text_to_be_present_in_element(selector, expected_text)
+    element = wait.until(condition)
+
+    select_elem = wait.until(EC.presence_of_element_located((By.XPATH, '//select[@name="cotizaciones_length"]')))
+    elem = Select(select_elem)
+    elem.select_by_visible_text('Todo')
+
+    table = driver.find_element_by_id('cotizaciones')
+    rows = table.find_elements(By.TAG_NAME, 'tr')
+
+    INDEX_TOTAL = 11
+    INDEX_SYMBOL = 0
+    INDEX_QUOTE = 1
+
+    headers = rows[0].find_elements_by_tag_name('td')
+    assert headers[INDEX_SYMBOL].text == 'Símbolo', f'unexpected header name [1]'
+    assert headers[INDEX_TOTAL].text == 'Monto\nOperado', f'unexpected header name [2]'
+
+    stocks = list()
+    for row in rows[1:-1]:
+        cols = row.find_elements_by_tag_name('td')
+
+        name = cols[INDEX_SYMBOL].text.split("\n")[0]
+        assert len(name) > 0, f'"{name}" too short'
+
+        volumen = int(cols[INDEX_TOTAL].text.replace('.', ''))
+        cotizacion=float(cols[INDEX_QUOTE].text.replace('.', '').replace(',', '.'))
+
+
+        stocks.append({'volumen': volumen, 'cotizacion': cotizacion, 'name': name})
+
+    driver.close()
+
+    stocks.sort(key=lambda x: x['volumen'],reverse=True)
+
+
+    return stocks
+
+
+
+
+if __name__ == "__main__":
+    options = FirefoxOptions()
+    #options.add_argument("--headless")
+    #firefox_path = GeckoDriverManager().install()
+    driver = webdriver.Firefox(options=options)#, executable_path=firefox_path)
+    base_url = 'https://www.invertironline.com/'
+    stocks_list_path = Path('stocks.json')
+    resultado= cotizacion_cedears(driver,base_url)
+
+    
+    driver.quit()
